5a5b5d6eef3fea43cb71ee3f735f7e07d721dc9c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-09 01:08:15 +0300

    the process of inserting new registrants into the SQL database has been completed.

--- Hospital_Project/FrmPatientSignUp.cs
+++ Hospital_Project/FrmPatientSignUp.cs
@@ -20,7 +20,16 @@ namespace Hospital_Project
         sqlconnect bgl = new sqlconnect();
         private void BtnSignup_Click(object sender, EventArgs e)
         {
-            SqlCommand komut = new SqlCommand("insert into Tbl_Patients(PatientName, PatientSurname, PatientSSN, PatientPhone, PatientPassword, PatientGender) values (@p1, @p2, @p3, @p4, @p5, @p6)" ,bgl.baglanti());
+            SqlCommand komut = new SqlCommand("insert into Tbl_Patients(PatientName, PatientSurname, PatientSSN, PatientPhone, PatientPassword, PatientGender) values (@p1, @p2, @p3, @p4, @p5, @p6)", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtName.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSurname.Text);
+            komut.Parameters.AddWithValue("@p3", MskTC.Text);
+            komut.Parameters.AddWithValue("@p4", MskPhone.Text);
+            komut.Parameters.AddWithValue("@p5", TxtPassword.Text);
+            komut.Parameters.AddWithValue("@p6", CmbGender.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Your registration has been successfully completed, Your password: " + TxtPassword.Text, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
