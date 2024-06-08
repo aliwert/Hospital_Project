ecc7c4cd1697cf6390d1f133066b30da501f63b4
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-09 01:49:58 +0300

    added subjects from sql

--- Hospital_Project/FrmPatientDetail.cs
+++ Hospital_Project/FrmPatientDetail.cs
@@ -23,14 +23,33 @@ namespace Hospital_Project
         private void FrmPatientDetail_Load(object sender, EventArgs e)
         {
             LblTC.Text = tc;
+            // name surname 
             SqlCommand komut = new SqlCommand("Select PatientName, PatientSurname From Tbl_Patients where PatientSSN=@p1", bgl.baglanti());
             komut.Parameters.AddWithValue("@p1", LblTC.Text);
             SqlDataReader sqlDataReader = komut.ExecuteReader();
-            while(sqlDataReader.Read()) {
+            while (sqlDataReader.Read())
+            {
                 LblNameSurname.Text = sqlDataReader[0] + " " + sqlDataReader[1];
 
             }
             bgl.baglanti().Close();
+
+            // appointment history
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Tbl_Appointments where PatientSSN= " + tc, bgl.baglanti());
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+
+            // subject
+
+            SqlCommand komut2 = new SqlCommand("Select SubjectName From Tbl_Subjects", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                CmbSubject.Items.Add(dr2[0]);
+            }
+            bgl.baglanti().Close();
+
         }
     }
 }
