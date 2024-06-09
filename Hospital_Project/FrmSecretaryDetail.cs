6c488804cfa712911cde60ffbb69e9b4f21d8e57
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-09 14:14:10 +0300

    subject information has been loaded into the datagrid

--- Hospital_Project/FrmSecretaryDetail.cs
+++ Hospital_Project/FrmSecretaryDetail.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Hospital_Project
 {
     public partial class FrmSecretaryDetail : Form
@@ -17,9 +17,29 @@ namespace Hospital_Project
             InitializeComponent();
         }
         public string TCnu;
+        sqlconnect bgl = new sqlconnect();
         private void FrmSecretaryDetail_Load(object sender, EventArgs e)
         {
             LblTC.Text = TCnu;
+
+            // name-surname
+            SqlCommand cmd = new SqlCommand("Select SecretaryNameSurname From Tbl_Secretary where SecretarySSN=@p1", bgl.baglanti());
+            cmd.Parameters.AddWithValue("@p1", LblTC.Text);
+            SqlDataReader dr1 = cmd.ExecuteReader();
+            while (dr1.Read())
+            {
+                LblNameSurname.Text= dr1[0].ToString();
+            }
+            bgl.baglanti().Close();
+
+
+            // add subject to datagrid
+
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select SubjectName from Tbl_Subjects", bgl.baglanti());
+            da.Fill(dt1);
+            dataGridView2.DataSource = dt1;
+
         }
     }
 }
